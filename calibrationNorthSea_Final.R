0f8f7f6157f43c62dad8ea4ad28dc183b15c4a2b
Mlla <mariella.canales@gmail.com>
2017-10-04 18:01:17 +1100

    This file is modified copy from Julia's dropbox folder. North Sea Size based model

--- calibrationNorthSea_Final.R
+++ calibrationNorthSea_Final.R
@@ -8,26 +8,29 @@
 rm(list=ls())
 library(minpack.lm)
 
-#- Set working directory of Dropbox folder
-#setwd("/Users/jlblanch/Dropbox/sizeBasedStuff/NorthSea")
+# - Set working directory of Dropbox folder
+# setwd("/Users/jlblanch/Dropbox/sizeBasedStuff/NorthSea")
 # setwd("D:/Repository/Dropbox/sizeBasedStuff/NorthSea")
-setwd("~/Dropbox/sizeBasedStuff/NorthSea")
+# setwd("/Users/Mlla/Dropbox/Size Spectrum Model Examples/3. Multispecies size spectrum model/multispecies_sizebasedmodels/NorthSea/R")
 
-source("R/paramNorthSeaModel.R")
-source("R/calibration_funcs.r")
-source("../R/SizeBasedModel.r")
-source("../R/SelectivityFuncs.r")
-source("../R/plots.r")
+# Mlla: The files for setup and project the model are all in the same folder now
+source("paramNorthSeaModel.R")
+source("calibration_funcs.r")
+source("SizeBasedModel.r")
+source("SelectivityFuncs.r")
+source("plots.r")
 
 #----------------------------------------------
 # 1) Load the parameter input data
 #----------------------------------------------
 
 #- interaction matrix
-load("input/interactionmatrix_Schoener_twostage2D.RData")
+load("interactionmatrix_Schoener_twostage2D.RData")
 
 #- parameter file
-paramfile               <- "input/IMAGE_paper_params_new.txt"
+# paramfile <- "IMAGE_paper_params_new.txt" # Mlla: Here I did not find this particular file within your folder, so 
+# I tried to run the code with a similar: ""IMAGE_paper_params_newNH.txt"
+paramfile <- "IMAGE_paper_params_newNH.txt"
 
 #----------------------------------------------
 # 2) Setup the parameter file
@@ -41,8 +44,12 @@ param$tmax              <- 1000
 param$dt                <- 1
 
 #- Get Rmax values
-source("R/getRmax.R")
-param$species$R0        <- Rmax$RmaxAB
+# source("R/getRmax.R") #Mlla: I commented this function becuase I did not find it. So instead I used a vector or Rmax from 
+# North sea data in MIZER examples. No quiet sure if the type of object is the same or will cause some problems later.
+# newRmax are in log scale
+# param$species$R0        <- Rmax$RmaxAB
+
+param$species$R0 <- c(26.52, 25.92, 24.22, 24.77, 22.41, 21.60, 19.41, 20.34, 22.45, 21.02, 17.73, 19.64)
 
 #----------------------------------------------
 # 3) Setup initial model
@@ -73,18 +80,25 @@ logParams               <- c(log(Initialcomm$param$species$R0),log(param$kap))
 # 5) Optimization call
 #----------------------------------------------
 count                 <- 0 #reset counting
-# optim_yield_ssb_kappa <- optim(par=logParams,fn = calibrateR0_yield_SSB_kappa_optim,
-# method="L-BFGS-B",hessian=F,
-# lower=c(rep(15,12),20),upper=c(rep(35,12),30),
-# Initialcomm = Initialcomm, meantsteps = meantsteps,
-# maxSSBpenalty=maxSSBpenalty, logerror = logerror,
-# paramfile=paramfile)
+# We modified the following lines becuase Julia used optim in the North Sea work.
 
-nlslm_yield_ssb_kappa <- nls.lm(par=logParams,fn = calibrateR0_yield_SSB_kappa_nls,
+# Mlla: the function that is call by optim function "calibrateR0_yield_SSB_kappa_optim", it is not in the folder calibration_func
+# but in the line 116. 
+
+optim_yield_ssb_kappa <- optim(par=logParams,fn = calibrateR0_yield_SSB_kappa_optim,
+                                method="L-BFGS-B",hessian=F,
                                 lower=c(rep(15,12),20),upper=c(rep(35,12),30),
                                 Initialcomm = Initialcomm, meantsteps = meantsteps,
                                 maxSSBpenalty=maxSSBpenalty, logerror = logerror,
                                 paramfile=paramfile)
+
+# Mlla: I am having an error in the optimization, and I am not able to create the output from optim function.
+
+# nlslm_yield_ssb_kappa <- nls.lm(par=logParams,fn = calibrateR0_yield_SSB_kappa_nls,
+#                                 lower=c(rep(15,12),20),upper=c(rep(35,12),30),
+#                                 Initialcomm = Initialcomm, meantsteps = meantsteps,
+#                                 maxSSBpenalty=maxSSBpenalty, logerror = logerror,
+#                                 paramfile=paramfile)
 
 # Nonlinear regression via the Levenberg-Marquardt algorithm
 # parameter estimates: 27.5837024746905, 27.1561754623063, 30.7349325423871, 27.3123297759941, 25.1961943832892, 25.7774081810535, 23.0108970612388, 31.1339477152861, 28.4660352897801, 26.2304390655457, 23.4165712287494, 27.8576277457183, 25.899867253659 
@@ -99,6 +113,7 @@ nlslm_yield_ssb_kappa <- nls.lm(par=logParams,fn = calibrateR0_yield_SSB_kappa_n
 #---------------------
 #- Calibration routine optim
 #---------------------
+
 calibrateR0_yield_SSB_kappa_optim <- function(logR0hat, Initialcomm, meantsteps=NA, maxSSBpenalty=FALSE, logerror = FALSE,paramfile="input/IMAGE_paper_params_new.txt")
 {
   #count <- count + 1
@@ -152,61 +167,61 @@ calibrateR0_yield_SSB_kappa_optim <- function(logR0hat, Initialcomm, meantsteps=
 }
 
 #---------------------
-#- Calibration routine optim
+#- Calibration routine optim #Mlla: I commented this function because is not used eaither
 #---------------------
 
-calibrateR0_yield_SSB_kappa_nls <- function(logR0hat=logParams, Initialcomm, meantsteps=NA, maxSSBpenalty=FALSE, logerror = FALSE,paramfile="input/IMAGE_paper_params_new.txt")
-{
-  #count <- count + 1
-  assign("count", count+1, pos = .GlobalEnv)
-  
-  kap <- exp(logR0hat[length(logR0hat)])
-  cat("count: ", count, ". logR0", signif(logR0hat,5), "\n")
-  
-  Newparam<-paramNSModel(fileSpecies=paramfile,
-                         theta = theta,
-                         kap=kap)
-  
-  Newparam$species$R0 <- exp(logR0hat[-length(logR0hat)])
-  
-  Newparam$theta      <- Initialcomm$param$theta
-  Newparam$tmax       <- Initialcomm$param$tmax
-  Newparam$dt         <- Initialcomm$param$dt
-  Newparam$wPPcut     <- Initialcomm$param$wPPcut
-  
-  # Project model with the new R0, continuing from where the initial run left off
-  Newcomm             <- Setup(Newparam, ContinueCalculation=T, initialcommunity=Initialcomm)
-  Newcomm             <- Project(Newcomm)
-  ye                  <- Yielderror(Newcomm,meantsteps=meantsteps, logerror = logerror)
-  se                  <- SSBerror(Newcomm,meantsteps=meantsteps, maxSSBpenalty=maxSSBpenalty, logerror = logerror)
-  error <- c(se,ye)
-  error <- error[!is.na(error)]
-  # error <- sqrt(error[!is.na(error)]^2) #Make sure the errors are positive
-  #Don't think we need this, nls.lm takes a vector of residuals and then computes the sum of squared errors 
-  
-  # Do we need to apply an extinction penalty?
-  extinct             <- Extinct_test(Newcomm,extinct_nsteps = 10)
-  if(extinct)
-  {
-    error          <- error * 1e1
-    cat("Extinction!\n")
-  }
-  
-  # Project model without fishing and apply an extinction penalty using new parameters and where the new model left off
-  Newparam$Q[,]       <- 0
-  Newcomm             <- Setup(Newparam, ContinueCalculation=T, initialcommunity=Newcomm)
-  Newcomm             <- Project(Newcomm)
-  
-  extinct2            <- Extinct_test(Newcomm,extinct_nsteps = 10)
-  if(extinct2)
-  {
-    error          <- error * 1e1
-    cat("Extinction!\n")
-  }
-  
-  cat("error: ", sum(error^2), "\n\n")
-  return(error)
-}
+# calibrateR0_yield_SSB_kappa_nls <- function(logR0hat=logParams, Initialcomm, meantsteps=NA, maxSSBpenalty=FALSE, logerror = FALSE,paramfile="input/IMAGE_paper_params_new.txt")
+# {
+#   #count <- count + 1
+#   assign("count", count+1, pos = .GlobalEnv)
+#   
+#   kap <- exp(logR0hat[length(logR0hat)])
+#   cat("count: ", count, ". logR0", signif(logR0hat,5), "\n")
+#   
+#   Newparam<-paramNSModel(fileSpecies=paramfile,
+#                          theta = theta,
+#                          kap=kap)
+#   
+#   Newparam$species$R0 <- exp(logR0hat[-length(logR0hat)])
+#   
+#   Newparam$theta      <- Initialcomm$param$theta
+#   Newparam$tmax       <- Initialcomm$param$tmax
+#   Newparam$dt         <- Initialcomm$param$dt
+#   Newparam$wPPcut     <- Initialcomm$param$wPPcut
+#   
+#   # Project model with the new R0, continuing from where the initial run left off
+#   Newcomm             <- Setup(Newparam, ContinueCalculation=T, initialcommunity=Initialcomm)
+#   Newcomm             <- Project(Newcomm)
+#   ye                  <- Yielderror(Newcomm,meantsteps=meantsteps, logerror = logerror)
+#   se                  <- SSBerror(Newcomm,meantsteps=meantsteps, maxSSBpenalty=maxSSBpenalty, logerror = logerror)
+#   error <- c(se,ye)
+#   error <- error[!is.na(error)]
+#   # error <- sqrt(error[!is.na(error)]^2) #Make sure the errors are positive
+#   #Don't think we need this, nls.lm takes a vector of residuals and then computes the sum of squared errors 
+#   
+#   # Do we need to apply an extinction penalty?
+#   extinct             <- Extinct_test(Newcomm,extinct_nsteps = 10)
+#   if(extinct)
+#   {
+#     error          <- error * 1e1
+#     cat("Extinction!\n")
+#   }
+#   
+#   # Project model without fishing and apply an extinction penalty using new parameters and where the new model left off
+#   Newparam$Q[,]       <- 0
+#   Newcomm             <- Setup(Newparam, ContinueCalculation=T, initialcommunity=Newcomm)
+#   Newcomm             <- Project(Newcomm)
+#   
+#   extinct2            <- Extinct_test(Newcomm,extinct_nsteps = 10)
+#   if(extinct2)
+#   {
+#     error          <- error * 1e1
+#     cat("Extinction!\n")
+#   }
+#   
+#   cat("error: ", sum(error^2), "\n\n")
+#   return(error)
+# }
 
 #---------------------
 #- Extinction routine
